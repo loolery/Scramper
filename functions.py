e002d4db920063bb49f5c9ed9e99a3f016fb04d9
loolery <royalbytes@gmail.com>
2023-03-07 10:14:59 +0100

    city search +
    UK, France, Spain and Italy added

--- functions.py
+++ functions.py
@@ -82,24 +82,40 @@ def search_teamlinks(url):
         team_link.append('https://www.transfermarkt.de' + a.get('href', None))
     return team_link
 
-def wiki_suche(url):
+def staedte_suche(url):
+    #sucht nach den Städten eines Landes, deren Einwohnerzahl und dem dazu gehörigen
+    #Bundeslandes, der Parameter muss die URL zur wikipediaseite mit den Daten sein.
+    # Rückgabe ist ein dictionary mit einer Liste drin: dic[1][1]
+    # im Falle UKs wird das Land noch mit angehängt also: dic[1][2]
     soup = soupobj(url)
     dictionary = {}
     chars = "0123456789,"
     # Suche die Tabelle im Quelltext
-    try: table = soup.find("table", {"class": "wikitable"})
+    try:
+        if 'Frankreich' in url: table = soup.find_all("table", {"class": "wikitable"})[2]
+        else: table = soup.find_all("table", {"class": "wikitable"})[1]
     except: result = None   #Tabelle konnte nicht gefunden werden.
     else:
-        #Suche nach den Daten in den Feldern 'Name' & '2021'
+        #Suche nach den Daten in den Feldern 'Name', 'Bundesland' 'aktuellste Einwohnerzahl'
         for row in table.tbody.find_all("tr"):
-            name_col = row.find("td", {"style": "text-align:left"})
+            name_col = row.find("a", title=True)
             if name_col is not None:
                 name = name_col.text.strip()
                 for char in chars:
                     name = name.replace(char, "")
                 data_cols = row.find_all("td")
-                einw_2021 = data_cols[-6].text.strip().replace('.', '')
-                bundesland = data_cols[-1].text.strip().split(u'\xa0')[0]
-                dictionary[name] = [bundesland, einw_2021]
+                if 'Vereinigten' in url:
+                    einw_2021 = data_cols[-3].text.strip().replace('.', '')
+                    bundesland = data_cols[-2].text.strip().split(u'\xa0')[0]
+                    staat = data_cols[-1].text.strip().split(u'\xa0')[0].replace('*', '')
+                    dictionary[name] = [bundesland, einw_2021, staat]
+                elif 'Deutschland' in url or 'Italien' in url:
+                    einw_2021 = data_cols[-2].text.strip().replace('.', '')
+                    bundesland = data_cols[-1].text.strip().split(u'\xa0')[0].replace('*', '')
+                    dictionary[name] = [bundesland, einw_2021]
+                elif 'Spanien' in url or 'Frankreich' in url:
+                    einw_2021 = data_cols[-3].text.strip().replace('.', '')
+                    bundesland = data_cols[-2].text.strip().split(u'\xa0')[0].replace('*', '')
+                    dictionary[name] = [bundesland, einw_2021]
                 result = dictionary.items()
     return result
